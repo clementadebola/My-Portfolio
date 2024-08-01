cf9fae2b63a10af6e8f86c20e813957183446f3f
clement <clementadebola54@gmail.com>
2024-08-01 05:04:01 +0100

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           {/* <img src={logo} className="img-fluid logo" alt="brand" />  */}
-          <h2 className="img-fluid logo" alt="brand">  AB</h2>
+          <h2 className="img-fluid logo" alt="brand"> AC</h2>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
