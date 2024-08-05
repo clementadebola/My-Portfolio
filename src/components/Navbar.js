317b22bd07e7df9f095657dff9b992cf50b70553
clement <clementadebola54@gmail.com>
2024-08-05 01:25:01 +0100

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,7 +105,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/clementadebola/My-Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
