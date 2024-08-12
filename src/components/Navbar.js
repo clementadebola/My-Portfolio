ae94a7150f8f543ca0ea0da13bae712e22644c81
clement <clementadebola54@gmail.com>
2024-08-12 22:34:01 +0100

    new chnages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          {/* <img src={logo} className="img-fluid logo" alt="brand" />  */}
+          {/* <img className="img-fluid logo" src={logo}  alt="brand" />  */}
           <h2 className="img-fluid logo" alt="brand"> AC</h2>
         </Navbar.Brand>
         <Navbar.Toggle
